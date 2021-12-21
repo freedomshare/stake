28984ad0f31a0187eedbaa77882c1c37fcb1ec15
Albert <r.albert.huang@gmail.com>
2021-12-21 18:06:59 +0800

    Support web3 stake lib

---
+++ lib/stake.ts
@@ -0,0 +1,55 @@
+import { MelandStakes__factory, MELD__factory } from '../contracts';
+import { Network, Web3Provider } from '@ethersproject/providers';
+import { BigNumber } from 'ethers';
+
+// 添加质押
+export async function stake(
+    stakeAddress: string,
+    provider: Web3Provider,
+    meldAddress: string,
+    stakePoolId: BigNumber
+) {
+    const melandStakes = MelandStakes__factory.connect(stakeAddress, provider.getSigner());
+    const meld = MELD__factory.connect(meldAddress, provider.getSigner());
+    const stakePoolInfo = await melandStakes.stakePoolById(stakePoolId);
+    const accounts = await provider.listAccounts();
+    const allowedAmount = await meld.allowance(accounts[0], stakeAddress);
+
+    if (allowedAmount.lt(stakePoolInfo.numberOfMELD)) {
+        const approveTx = await meld.approve(stakeAddress, BigNumber.from('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'))
+        await approveTx.wait();
+    }
+
+    const stakeTx = await melandStakes.stake(stakePoolId);
+    return stakeTx.wait();
+}
+
+// 获取当前最高质押等级.
+export async function getHighestLevelStakeInfo(
+    stakeAddress: string,
+    provider: Web3Provider,
+) {
+    const melandStakes = MelandStakes__factory.connect(stakeAddress, provider.getSigner());
+    const accounts = await provider.listAccounts();
+    const highStakeInfo = await melandStakes.getUserCurrentHighLevelStake(accounts[0]);
+    return melandStakes.stakePoolById(highStakeInfo.stakePoolId);
+}
+
+// 获取所有待获取的利息
+export async function getAllEarned(
+    stakeAddress: string,
+    provider: Web3Provider
+) {
+    const melandStakes = MelandStakes__factory.connect(stakeAddress, provider.getSigner());
+    return melandStakes.getStakerAllEarned();
+}
+
+// 收取利息
+export async function harvest(
+    stakeAddress: string,
+    provider: Web3Provider
+) {
+    const melandStakes = MelandStakes__factory.connect(stakeAddress, provider.getSigner());
+    const tx = await melandStakes.harvest();
+    return tx.wait();
+}
